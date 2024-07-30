8100e26c7d38832551c4d48769b5c31fc383eaba
Fatima Alam <71816449+alamfatima1999@users.noreply.github.com>
2024-07-30 09:19:38 +0530

    Update Login.js

--- src/Components/LoginRegisterContainer/Login.js
+++ src/Components/LoginRegisterContainer/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 import './Login.css';
